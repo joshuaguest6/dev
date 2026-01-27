f48ff94c4189a86ed21cf4e008400b70a9d932de
Joshua Guest <joshuaguest6@gmail.com>
2026-01-27 14:35:42 +1000

    fix # removed today logic and nulls in vehicle name

--- metroford_202601/app/app.py
+++ metroford_202601/app/app.py
@@ -3,6 +3,10 @@ import pandas as pd
 from google.cloud import storage
 import json
 from datetime import datetime, timedelta
+import pytz
+
+tz = pytz.timezone('Australia/Sydney')
+today = datetime.now(tz)
 
 st.set_page_config(page_title="Inventory Tracking and Change Detection - Metro Ford", layout='wide')
 
@@ -57,15 +61,15 @@ removed_df = get_removed_data()
 
 def highlight_changes(row):
     color = ''
-    cutoff_date = (datetime.now() - timedelta(days=7)).date()
+    cutoff_date = (today - timedelta(days=7)).date()
     if row['Last Change Date'] is not None and pd.to_datetime(row['Last Change Date']).date() > cutoff_date:
         color = 'background-color: #615fff33'
     return [color] * len(row)
 
 def highlight_removed(row):
     color = ''
-    cutoff_date = datetime.now().date()
-    if pd.to_datetime(row['Removed Date']).date() == cutoff_date:
+    cutoff_date = today.date()
+    if row['Removed Date'] == cutoff_date:
         color = 'background-color: #615fff33'
     return [color] * len(row)
 
@@ -73,30 +77,36 @@ st.title('Inventory Tracking and Change Detection - Metro Ford')
 
 st.markdown("### Inventory")
 
-df['Vehicle'] = df['Year'].astype(str) + ' ' + df['Make'] + ' ' + df['Model'] + ' ' + df['Trim']
+df['Vehicle'] = df.apply(
+    lambda x: ' '.join(
+        [str(c) for c in [x['Year'], x['Make'], x['Model'], x['Trim']] if pd.notna(c)]
+    ),
+    axis=1
+)
 df['Price'] = df['Price'].map('${:,.0f}'.format)
 
 
-if not df.empty and pd.notna(df['Date'].iloc[0]):
-    today = pd.to_datetime(df['Date'].iloc[0]).strftime('%Y-%m-%d')
-else:
-    today = '—'
+removed_df['Date'] = pd.to_datetime(removed_df['Date']).dt.date
 
 cols = st.columns([1,3])
 with cols[0]:
-    st.metric('Today', today)
+    st.metric('Today', today.strftime('%Y-%m-%d') )
     st.metric('\\# Vehicles', len(df))
     st.metric('\\# Changes Today', df['Status'].notna().sum())
-    st.metric('\\# Removed Today', len(removed_df))
+    st.metric('\\# Removed Today', len(removed_df[removed_df['Date']==today.date()]))
 
 with cols[1]:
     st.markdown('Highlights indicate changes in the past 7 days')
     st.dataframe(df[['Vehicle', 'Condition', 'Price', 'Status', 'Last Change','Last Change Date', 'VIN']].sort_values(by='Last Change Date', ascending=False).style.apply(highlight_changes, axis=1))
 
 st.markdown("### Removed")
-removed_df['Vehicle'] = removed_df['Year'].astype(str) + ' ' + removed_df['Make'] + ' ' + removed_df['Model'] + ' ' + removed_df['Trim']
+removed_df['Vehicle'] = removed_df.apply(
+    lambda x: ' '.join(
+        [str(c) for c in [x['Year'], x['Make'], x['Model'], x['Trim']] if pd.notna(c)]
+    ),
+    axis=1
+)
 removed_df['Price'] = removed_df['Price'].map('${:,.0f}'.format)
-removed_df['Date'] = pd.to_datetime(removed_df['Date']).dt.date
 removed_df = removed_df.rename(columns={'Date': 'Removed Date'})
 
 st.markdown('Highlights indicate vehicles removed today')
@@ -104,7 +114,12 @@ st.dataframe(removed_df[['Removed Date', 'Vehicle', 'Condition', 'Price', 'VIN']
 
 st.markdown("### Summary")
 
-summary_df['Vehicle'] = summary_df['Year'].astype(str) + ' ' + summary_df['Make'] + ' ' + summary_df['Model']
+summary_df['Vehicle'] = summary_df.apply(
+    lambda x: ' '.join(
+        [str(c) for c in [x['Year'], x['Make'], x['Model']] if pd.notna(c)]
+    ),
+    axis=1
+)
 
 # Format price nicely
 summary_df['avg_price'] = summary_df['avg_price'].map('${:,.0f}'.format)
