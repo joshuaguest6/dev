6f91ba47fcaa534ce1c31587d1abe40a79cdead3
Joshua Guest <joshuaguest6@gmail.com>
2026-02-03 13:43:53 +1000

    change to headless

--- web_scraping/googlemaps_202601/main.py
+++ web_scraping/googlemaps_202601/main.py
@@ -49,7 +49,7 @@ def parse_results(articles):
 def generate_list(search):
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=False,
+            headless=True,
             args=["--disable-blink-features=AutomationControlled"]
         )
         context = browser.new_context()
