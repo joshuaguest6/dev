300315768e146455d92e215113630b0b21ff58ba
Joshua Guest <joshuaguest6@gmail.com>
2026-02-12 13:23:32 +1000

    log websites and phones found

--- web_scraping/googlemaps_202601/main.py
+++ web_scraping/googlemaps_202601/main.py
@@ -166,6 +166,9 @@ def extract_details(records):
             context.close()
             time.sleep(random.uniform(2, 5))
 
+        logging.info(f'{len([record for record in records if record['website_link'] is not None])} website links found')
+        logging.info(f'{len([record for record in records if record['website_link'] is None])} website links not found')
+
         browser.close()
     
     return records
@@ -207,6 +210,9 @@ def enrich_data(records):
 
         logging.info('Finished visiting business websites...')
         
+        logging.info(f'{len([record for record in records if record['website_phone'] is not None])} phone numbers found')
+        logging.info(f'{len([record for record in records if record['website_phone'] is None])} phone numbers not found')
+
         browser.close()
 
     return records
